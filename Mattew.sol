713df1f2ef393281be43deb2e7a93a54553fcba0
Roland Kofler <roland.kofler@gmail.com>
2016-11-18 17:10:49 +0100

    The final Contract

--- Mattew.sol
+++ Mattew.sol
@@ -1,53 +1,15 @@
 pragma solidity ^0.4.4;
 
-/**
- * ## Mattew - a contract for increasing "whaleth"
- * 
- * The rich get richer, 
- * the WHALE get WHALER
- * ╭━━━━━━━━╮┏━╮╭━┓
- * ┃┈┈┈┈┈┈┈┈┃╰╮╰╯╭╯
- * ┃╰╯┈┈┈┈┈┈╰╮╰╮╭╯┈
- * ┣━━╯┈┈┈┈┈┈╰━╯┃┈┈
- * ╰━━━━━━━━━━━━╯┈┈
- * 
- * In sociology, the Matthew effect (or accumulated advantage) 
- * is the phenomenon where "the rich get richer and the poor get poorer. 
- * - https://en.wikipedia.org/wiki/Matthew_effect 
- *
- * This contract helps with this process by rewarding the richest player.
- *
- * ### Description 
- * 
- * 1. A player sends ether to the contract address
- * 2a. If the amount is too low it fails
- * 2b. If the amount at least +1 ETH is higher than the STAKE
- *      and send within PERIOD since the last pay-in
- *      then the sender receives the last STAKE but pays in his STAKE.
- *      
- *                      
- *                      1ΞΞΞΞΞΞΞΞΞ*Period over
- *              1ΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞ
- *           2ΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞ      1ΞΞΞΞΞΞΞΞΞ*Period over
- *           ΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞ  1ΞΞΞΞΞΞΞΞΞΞΞΞΞ
- *      1ΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞ1ΞΞΞΞΞΞΞΞΞΞΞΞΞΞΞ1ΞΞΞΞΞ...
- * 
- *      Figure: The STAKE gets higher until nobody pays in for > PERIOD
- * 
- * 2c. If the grace PERIOD is over and nobody increased the STAKE, 
- *      the last STAKE wins all the Ether
- * 3. Contract gets destroyed Experiment over.
- *
- * MIT LICENSE 2016 Roland Kofler, thanks to Crul for testing
- **/
-
+// ## Mattew - a contract for increasing "whaleth"
+// README: https://github.com/rolandkofler/mattew
+// MIT LICENSE 2016 Roland Kofler, thanks to Crul for testing
 
 contract Mattew {
     address whale;
     uint256 stake;
     uint256 blockheight;
-    uint256 constant BLOCKS_PER_DAY= uint256(60 * 60 * 24 /14);
-    uint256 constant PERIOD = 20; //60 * 10 /14; //BLOCKS_PER_DAY;
+    uint256 constant PERIOD = 200; //60 * 10 /14; //BLOCKS_PER_DAY;
+    uint constant DELTA = 0.1 ether;
     
     event MattewWon(string msg, address winner, uint value,  uint blocknumber);
     event StakeIncreased(string msg, address staker, uint value, uint blocknumber);
@@ -68,12 +30,12 @@ contract Mattew {
             bool isSuccess = whale.send(stake);
             MattewWon("Mattew won (mattew, stake, blockheight)", whale, stake, block.number);
             setFacts();
-            selfdestruct(whale); //a gesture of responsibility 
+            // selfdestruct(whale); People with Ethereum Foundation are ok with it.
             return;
             
         }else{
             
-            if (msg.value < stake + 1 ether) throw;
+            if (msg.value < stake + DELTA) throw;
             bool isOtherSuccess = msg.sender.send(stake);
             setFacts();
             StakeIncreased("stake increased (whale, stake, blockheight)", whale, stake, blockheight);
