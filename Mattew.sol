78ec2588531206d2653bb76919b561c92e904124
Roland Kofler <roland.kofler@gmail.com>
2016-11-19 16:10:52 +0100

    Update Mattew.sol

--- Mattew.sol
+++ Mattew.sol
@@ -5,52 +5,70 @@ pragma solidity ^0.4.4;
 // MIT LICENSE 2016 Roland Kofler, thanks to Crul for testing
 
 contract Mattew {
+    address owner;
     address whale;
-    uint256 stake;
     uint256 blockheight;
-    uint256 constant PERIOD = 200; //60 * 10 /14; //BLOCKS_PER_DAY;
+    uint256 period = 5; //BLOCKS_PER_DAY;
     uint constant DELTA = 0.1 ether;
+    bool mustBeDestroyed = false;
+    uint newPeriod = 5;
     
     event MattewWon(string msg, address winner, uint value,  uint blocknumber);
     event StakeIncreased(string msg, address staker, uint value, uint blocknumber);
     
     function Mattew(){
+        owner = msg.sender;
         setFacts();
     }
     
     function setFacts() private {
-        stake = msg.value;
+        period = newPeriod;
         blockheight = block.number;
         whale = msg.sender;
     }
     
     /// The rich get richer, the whale get whaler
     function () payable{
-        if (block.number - PERIOD > blockheight){
-            bool isSuccess = whale.send(stake);
-            MattewWon("Mattew won (mattew, stake, blockheight)", whale, stake, block.number);
+        if (block.number - period >= blockheight){
+            bool isSuccess = whale.send(this.balance - DELTA);
+            MattewWon("Mattew won", whale, this.balance, block.number);
             setFacts();
-            // selfdestruct(whale); People with Ethereum Foundation are ok with it.
+            if (mustBeDestroyed) selfdestruct(whale); 
             return;
             
         }else{
             
-            if (msg.value < stake + DELTA) throw;
-            bool isOtherSuccess = msg.sender.send(stake);
+            if (msg.value < this.balance + DELTA) throw;
+            bool isOtherSuccess = msg.sender.send(this.balance);
             setFacts();
-            StakeIncreased("stake increased (whale, stake, blockheight)", whale, stake, blockheight);
+            StakeIncreased("stake increased", whale, this.balance, blockheight);
         }
     }
     
-    
-    function getStake() public constant returns(uint){
-        return stake;
+    // better safe than sorry
+    function destroyWhenRoundOver() onlyOwner{
+        mustBeDestroyed = true;
     }
     
+    // next round we set a new staking perioud
+    function setNewPeriod(uint _newPeriod) onlyOwner{
+        newPeriod = _newPeriod;
+    }
+    
+    function getPeriod() constant returns (uint){
+        period;
+    }
+    
+    //how long until a Mattew wins?
     function getBlocksTillMattew() public constant returns(uint){
-        if (blockheight + PERIOD > block.number)
-            return blockheight + PERIOD - block.number;
+        if (blockheight + period > block.number)
+            return blockheight + period - block.number;
         else
             return 0;
+    }
+    
+    modifier onlyOwner(){
+        if (msg.sender != owner) throw;
+        _;
     }
 }
